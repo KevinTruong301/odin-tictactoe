d83ce191c2fe3108ea9de8083146f411503aedf4
Kevin <kevintruong30195@gmail.com>
2024-11-18 15:03:46 -0800

    added tic tac toe grid and integrated with game logic

--- tictactoe.js
+++ tictactoe.js
@@ -15,15 +15,32 @@
 function TicTacToe()
 {
     let board = [[],[],[]];
+    let turn = true; //true is X false is O
 
-    function PlayerMove(player, posX, posY)
+    function GetPlayerTurn()
     {
+        return turn;
+    }
+
+    function PlayerMove(posX, posY)
+    {
+        let player = 'x';
+        if(turn)
+        {
+            player = 'x';
+        }
+        else
+        {
+            player = 'o';
+        }
+        turn = !turn;
         board[posX][posY] = player;
-        CheckWin(player, posX, posY);
+        return CheckWin(player, posX, posY);
     }
 
     function CheckWin(player, posX, posY)
     {
+        console.log(player + " " + posX + " " + posY);
         //board check x
         let row = 0;
         let column = 0;
@@ -62,19 +79,53 @@ function TicTacToe()
                 antiDiag += 1;
             }
         }
-
         return row == 3 || column == 3 || diag == 3 || antiDiag == 3;
     }
 
-    return {PlayerMove, CheckWin};
+    function ResetBoard()
+    {
+        board = [[],[],[]];
+    }
+
+    return {PlayerMove, CheckWin, GetPlayerTurn};
 }
 
-let game = TicTacToe();
+let ticTacToe = TicTacToe();
 
-game.PlayerMove('x', 0, 1);
-game.PlayerMove('o', 0, 0);
-game.PlayerMove('x', 1, 1);
-game.PlayerMove('o', 1, 0);
-game.PlayerMove('x', 2, 1);
+function PlayerMove(evt){
+    if(evt.currentTarget.textContent != "X" && evt.currentTarget.textContent != "O")
+    {
+        evt.currentTarget.textContent = ticTacToe.GetPlayerTurn() ? "X" : "O";
+        let isWin = ticTacToe.PlayerMove(evt.currentTarget.posX, evt.currentTarget.posY);
+        console.log(isWin);
+    }
+}
 
+function CreateSquare(posX, posY){
 
+    let tictactoeGrid = document.querySelector("#tictactoe-grid");
+
+    let square = document.createElement("div");
+
+    square.style.width = "100px";
+    square.style.height = "100px";
+    square.style.border = "black solid 1px";
+    
+    square.addEventListener("click", PlayerMove);
+
+    square.posX = posX;
+    square.posY = posY;
+
+    tictactoeGrid.appendChild(square);
+}
+
+function PopulateTicTacToeGrid(){
+    for(let i = 0; i < 3; i++)
+    {
+        for(let j = 0; j < 3; j++)
+        {
+            CreateSquare(i, j);
+        }
+    }
+}
+PopulateTicTacToeGrid();
